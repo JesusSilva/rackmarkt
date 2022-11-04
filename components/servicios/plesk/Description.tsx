f6761b9ec9b64e99642e8e21397d24072db7d598
Jesús Silva Méndez <jesus.silva@ghop.es>
2022-11-04 01:27:12 +0100

    feat(plesk): seccion terminada

---
+++ components/servicios/plesk/Description.tsx
@@ -0,0 +1,99 @@
+export default function Description() {
+  return (
+    <div className="overflow-hidden bg-gray-100 py-16 px-4 sm:px-6 lg:px-8 xl:py-36">
+      <div className="mx-auto max-w-screen-2xl">
+        <div className="relative z-10 mb-8 md:mb-2 md:px-6">
+          <div className="max-w-prose text-base lg:max-w-none">
+            <p className="mt-2 text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+              Web hosting y WebOps cómodo y seguro.
+            </p>
+          </div>
+        </div>
+        <div className="relative">
+          <svg
+            className="absolute top-0 right-0 -mt-20 -mr-20 hidden md:block md:[overflow-anchor:none]"
+            width={404}
+            height={384}
+            fill="none"
+            viewBox="0 0 404 384"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="95e8f2de-6d30-4b7e-8159-f791729db21b"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width={404}
+              height={384}
+              fill="url(#95e8f2de-6d30-4b7e-8159-f791729db21b)"
+            />
+          </svg>
+          <svg
+            className="absolute bottom-0 left-0 -mb-20 -ml-20 hidden md:block md:[overflow-anchor:none]"
+            width={404}
+            height={384}
+            fill="none"
+            viewBox="0 0 404 384"
+            aria-hidden="true"
+          >
+            <defs>
+              <pattern
+                id="7a00fe67-0343-4a3c-8e81-c145097a3ce0"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width={404}
+              height={384}
+              fill="url(#7a00fe67-0343-4a3c-8e81-c145097a3ce0)"
+            />
+          </svg>
+          <div className="relative md:bg-gray-100 md:p-6">
+            <div className="prose prose-lg prose-indigo text-gray-500 lg:max-w-none">
+              <p>
+                Plesk es uno de los paneles de administración de servidores más completos.
+                Diseñado para facilitar la vida de los profesionales, incluyendo
+                proveedores de hosting, desarrolladores y diseñadores, es útil a la hora
+                de desplegar y automatizar aplicaciones web o administrar empresas de
+                alojamiento.
+              </p>
+              <p>
+                Dispone de una interfaz web completa y amigable desde la que administrar y
+                automatizar de forma muy visual todos los parametros de tu servicio de
+                hosting, tanto si eres proveedor o reseller como para un usuario final.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- pages/servicios/plesk.tsx
+++ pages/servicios/plesk.tsx
@@ -1,5 +1,19 @@
+import Description from '../../components/servicios/plesk/Description'
+import Hero from '../../components/servicios/plesk/Hero'
+import Pricing from '../../components/servicios/plesk/Pricing'
+import Variety from '../../components/servicios/plesk/Variety'
+import FAQ from '../../components/shared/FAQ'
+
 function plesk() {
-  return <div>plesk</div>
+  return (
+    <>
+      <Hero />
+      <Description />
+      <Variety />
+      <Pricing />
+      <FAQ />
+    </>
+  )
 }
 
 export default plesk
