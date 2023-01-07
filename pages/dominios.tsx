f66a433925205e1e3f77c43fee653fd37b952247
Jesus Silva Mendez <soy@jesussilva.es>
2023-01-08 00:55:54 +0100

    fix(dominios): importacion del hero eliminada

--- pages/dominios.tsx
+++ pages/dominios.tsx
@@ -1,6 +1,5 @@
 import CategoriesDominios from '../components/dominios/Categories'
 import Free from '../components/dominios/Free'
-import HeroDominios from '../components/dominios/Hero'
 import Offers from '../components/dominios/Offers'
 import SearchEngine from '../components/dominios/SearchEngine'
 import TextDominios from '../components/dominios/Text'
